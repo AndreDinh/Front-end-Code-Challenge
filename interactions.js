80ef77d962dbb43e390d22484dbcdb15136b28f3
Andre Dinh <dinh.andre@yahoo.com>
2015-05-16 22:28:32 -0700

    created the toString method and clean up code looks

--- interactions.js
+++ interactions.js
@@ -1,16 +1,13 @@
-// $('.add').on('click', function(){
-//   $('ol').add('<li> with this random thing</li>');
-// });
 $(function() {
-$('#add').on('click', function(e){
+  $('#add').on('click', function(e) {
     e.preventDefault();
     var newText = $('input:text').val();
     $('li:last').after('<li>' + newText + '</li>');
     $textInput.val('');
 
   });
-$('li').on('click',function(){
-  (this).remove();
+  $('li').on('click', function() {
+    (this).remove();
   });
 
 });
--- libary.js
+++ libary.js
@@ -1,34 +1,30 @@
-function book(title,author){
-  this.title = title||'New Tittle';
-  this.author = author||'New Author';
-}
-var shelf = [];
-var mobyDick = new book(undefined,"author");
-shelf.add(mobyDick);
-shelf.remove(mobydick);
-shelf.remove(of_mice_and_men);
 
-Book.prototype.setinfo = function setinfo(title,author){
-  if(title !== undefined){
+function book(title, author) {
+  this.title = title || 'New Tittle';
+  this.author = author || 'New Author';
+}
+
+Book.prototype.setinfo = function setinfo(title, author) {
+  if (title !== undefined) {
     this.title = title;
   }
-  if(author !== undefined){
+  if (author !== undefined) {
     this.author = author;
   }
 }
 
-function add(){
+function add() {
   return book;
 }
-function remove(book){
-  if(book == undefined){
+
+function remove(book) {
+  if (book == undefined) {
     console.log("Book is not found");
-  }else{
+  } else {
     return false;
   }
 }
-function toString(){
-  //reads the book
-  return title;
-  return author;
+
+function toString() {
+  return [title, author];
 }
